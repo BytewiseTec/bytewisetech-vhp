2feab21b6ff41585d1e2c6cb974e21478a704115
muhammadkh4n <muhammadkh4n@gmail.com>
2024-09-26 18:00:17 +0500

    Added contact form email sending

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,43 @@
+'use client'
+
+import { sendMail } from '@/utils/send-mail'
+import { useForm } from 'react-hook-form'
+import { toast, Toaster } from 'sonner'
+
 export default function ContactPage() {
+  const { register, handleSubmit, formState } = useForm({
+    defaultValues: {
+      name: '',
+      email: '',
+      phone: '',
+      companyName: '',
+      message: '',
+    }
+  })
+
+  const isLoading = formState.isSubmitting
+
+  const onSubmit = async (values: any) => {
+    const response = await sendMail({
+      email: values.email,
+      subject: 'New lead from site',
+      text: `Name: ${values.name}\nPhone: ${values.phone}\nCompany: ${values.companyName}\nEmail: ${values.email}\nMessage: ${values.message}`,
+      html: `
+        <h1>New Lead From Site</h1>
+        <p><strong>Name:</strong> ${values.name}</p>
+        <p><strong>Phone:</strong> ${values.phone}</p>
+        <p><strong>Company:</strong> ${values.companyName}</p>
+        <p><strong>Email:</strong> ${values.email}</p>
+        <p><strong>Message:</strong> ${values.message}</p>
+      `,
+    })
+
+    if (response?.messageId) {
+      toast.success('Message Sent Successfully.')
+    } else {
+      toast.error('Failed To send message.')
+    }
+  }
   return (
     <>
       <section
@@ -70,51 +109,54 @@ export default function ContactPage() {
           <div className="section_space pb-0">
             <div className="row justify-content-lg-between">
               <div className="col-lg-7">
-                <div className="contact_form mb-0">
-                  <h3 className="details_item_info_title mb-1">Send Us A Message</h3>
-                  <p className="mb-5">
-                    Give us chance to serve and bring magic to your brand.
-                  </p>
-                  <div className="row">
-                    <div className="col-md-6">
-                      <div className="form-group">
-                        <label className="input_title" htmlFor="input_name">Full Name
-                        </label>
-                        <input id="input_name" className="form-control" type="text" name="name" placeholder="Jhon Doe" required />
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <div className="contact_form mb-0">
+                    <h3 className="details_item_info_title mb-1">Send Us A Message</h3>
+                    <p className="mb-5">
+                      Give us chance to serve and bring magic to your brand.
+                    </p>
+                    <div className="row">
+                      <div className="col-md-6">
+                        <div className="form-group">
+                          <label className="input_title" htmlFor="input_name">Full Name
+                          </label>
+                          <input id="input_name" className="form-control" type="text" {...register('name')} placeholder="Jhon Doe" required />
+                        </div>
                       </div>
-                    </div>
-                    <div className="col-md-6">
-                      <div className="form-group">
-                        <label className="input_title" htmlFor="input_email">Your Email
-                        </label>
-                        <input id="input_email" className="form-control" type="email" name="email" placeholder="doe@example.com" required />
+                      <div className="col-md-6">
+                        <div className="form-group">
+                          <label className="input_title" htmlFor="input_email">Your Email
+                          </label>
+                          <input id="input_email" className="form-control" type="email" {...register('email')} placeholder="doe@example.com" required />
+                        </div>
                       </div>
-                    </div>
-                    <div className="col-12">
-                      <div className="form-group">
-                        <label className="input_title" htmlFor="input_phone">Your Phone</label>
-                        <input id="input_phone" className="form-control" type="tel" name="phone" placeholder="+8250-3560 6565" />
+                      <div className="col-12">
+                        <div className="form-group">
+                          <label className="input_title" htmlFor="input_phone">Your Phone</label>
+                          <input id="input_phone" className="form-control" type="tel" {...register('phone')} placeholder="+8250-3560 6565" />
+                        </div>
                       </div>
-                    </div>
-                    <div className="col-12">
-                      <div className="form-group">
-                        <label className="input_title" htmlFor="input_textarea">Message</label>
-                        <textarea id="input_textarea" className="form-control" name="message" placeholder="How can we help you?" />
+                      <div className="col-12">
+                        <div className="form-group">
+                          <label className="input_title" htmlFor="input_textarea">Message</label>
+                          <textarea id="input_textarea" className="form-control" {...register('message')} placeholder="How can we help you?" />
+                        </div>
+                        <button type="submit" className="btn btn-primary" disabled={isLoading}>
+                          <span className="btn_label" data-text="Send Message">Send Message</span>
+                          <span className="btn_icon">
+                            <i className="fa-solid fa-arrow-up-right"></i>
+                          </span>
+                        </button>
                       </div>
-                      <button type="submit" className="btn btn-primary">
-                        <span className="btn_label" data-text="Send Message">Send Message</span>
-                        <span className="btn_icon">
-                          <i className="fa-solid fa-arrow-up-right"></i>
-                        </span>
-                      </button>
                     </div>
                   </div>
-                </div>
+                </form>
               </div>
             </div>
           </div>
         </div>
       </section>
+      <Toaster />
     </>
   )
 }
--- components/ContactUs/index.tsx
+++ components/ContactUs/index.tsx
@@ -1,4 +1,44 @@
+'use client'
+
+import { sendMail } from '@/utils/send-mail'
+import { useForm } from 'react-hook-form'
+import { Toaster, toast } from 'sonner'
+
 export default function ContactUs() {
+  const { register, handleSubmit, formState } = useForm({
+    defaultValues: {
+      name: '',
+      email: '',
+      phone: '',
+      companyName: '',
+      message: '',
+    }
+  })
+
+  const isLoading = formState.isSubmitting
+
+  const onSubmit = async (values: any) => {
+    const response = await sendMail({
+      email: values.email,
+      subject: 'New lead from site',
+      text: `Name: ${values.name}\nPhone: ${values.phone}\nCompany: ${values.companyName}\nEmail: ${values.email}\nMessage: ${values.message}`,
+      html: `
+        <h1>New Lead From Site</h1>
+        <p><strong>Name:</strong> ${values.name}</p>
+        <p><strong>Phone:</strong> ${values.phone}</p>
+        <p><strong>Company:</strong> ${values.companyName}</p>
+        <p><strong>Email:</strong> ${values.email}</p>
+        <p><strong>Message:</strong> ${values.message}</p>
+      `,
+    })
+
+    if (response?.messageId) {
+      toast.success('Application Submitted Successfully.')
+    } else {
+      toast.error('Failed To send application.')
+    }
+  }
+
   return (
     <section className="contact_section pb-80 bg-light section_decoration">
       <div className="container">
@@ -64,63 +104,65 @@ export default function ContactUs() {
             </div>
           </div>
           <div className="col-lg-8">
-            <div className="instant_contact_form">
-              <div className="small_title">
-                <i className="fa-solid fa-envelope-open-text"></i>
-                Let&apos;s Connect!
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="instant_contact_form">
+                <div className="small_title">
+                  <i className="fa-solid fa-envelope-open-text"></i>
+                  Let&apos;s Connect!
+                </div>
+                <h3 className="form_title">
+                  Send us a message, and we&apos;ll promptly discuss your project with you.
+                </h3>
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <label className="input_title" htmlFor="input_name">
+                        <i className="fa-regular fa-user"></i>
+                      </label>
+                      <input id="input_name" className="form-control" type="text" {...register('name')} placeholder="Your Name" required />
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <label className="input_title" htmlFor="input_email">
+                        <i className="fa-regular fa-envelope"></i>
+                      </label>
+                      <input id="input_email" className="form-control" type="email" {...register('email')} placeholder="Your Enter" required />
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <label className="input_title" htmlFor="input_phone">
+                        <i className="fa-regular fa-phone-volume"></i>
+                      </label>
+                      <input id="input_phone" className="form-control" type="tel" {...register('phone')} placeholder="Your Phone No." required />
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <label className="input_title" htmlFor="input_company">
+                        <i className="fa-regular fa-globe"></i>
+                      </label>
+                      <input id="input_company" className="form-control" type="text" {...register('companyName')} placeholder="Your Company Name" />
+                    </div>
+                  </div>
+                  <div className="col-12">
+                    <div className="form-group">
+                      <label className="input_title" htmlFor="input_textarea">
+                        <i className="fa-regular fa-comments"></i>
+                      </label>
+                      <textarea id="input_textarea" className="form-control" {...register('message')} placeholder="How can we help you?"></textarea>
+                    </div>
+                    <button type="submit" className="btn btn-primary" disabled={isLoading}>
+                      <span className="btn_label" data-text="Send Request">Send Request</span>
+                      <span className="btn_icon">
+                        <i className="fa-solid fa-arrow-up-right"></i>
+                      </span>
+                    </button>
+                  </div>
+                </div>
               </div>
-              <h3 className="form_title">
-                Send us a message, and we&apos;ll promptly discuss your project with you.
-              </h3>
-              <div className="row">
-                <div className="col-md-6">
-                  <div className="form-group">
-                    <label className="input_title" htmlFor="input_name">
-                      <i className="fa-regular fa-user"></i>
-                    </label>
-                    <input id="input_name" className="form-control" type="text" name="name" placeholder="Your Name" required />
-                  </div>
-                </div>
-                <div className="col-md-6">
-                  <div className="form-group">
-                    <label className="input_title" htmlFor="input_email">
-                      <i className="fa-regular fa-envelope"></i>
-                    </label>
-                    <input id="input_email" className="form-control" type="email" name="email" placeholder="Your Enter" required />
-                  </div>
-                </div>
-                <div className="col-md-6">
-                  <div className="form-group">
-                    <label className="input_title" htmlFor="input_phone">
-                      <i className="fa-regular fa-phone-volume"></i>
-                    </label>
-                    <input id="input_phone" className="form-control" type="tel" name="phone" placeholder="Your Phone No." required />
-                  </div>
-                </div>
-                <div className="col-md-6">
-                  <div className="form-group">
-                    <label className="input_title" htmlFor="input_company">
-                      <i className="fa-regular fa-globe"></i>
-                    </label>
-                    <input id="input_company" className="form-control" type="text" name="companyname" placeholder="Your Company Name" />
-                  </div>
-                </div>
-                <div className="col-12">
-                  <div className="form-group">
-                    <label className="input_title" htmlFor="input_textarea">
-                      <i className="fa-regular fa-comments"></i>
-                    </label>
-                    <textarea id="input_textarea" className="form-control" name="message" placeholder="How can we help you?"></textarea>
-                  </div>
-                  <button type="submit" className="btn btn-primary">
-                    <span className="btn_label" data-text="Send Request">Send Request</span>
-                    <span className="btn_icon">
-                      <i className="fa-solid fa-arrow-up-right"></i>
-                    </span>
-                  </button>
-                </div>
-              </div>
-            </div>
+            </form>
           </div>
         </div>
       </div>
@@ -130,6 +172,7 @@ export default function ContactUs() {
       <div className="decoration_item shape_image_2">
         <img src="/assets/images/shapes/shape_line_6.svg" alt="Techco Shape" />
       </div>
+      <Toaster />
     </section>
   )
 }
