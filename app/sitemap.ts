2a65aaa9f8a2725e75dbbe81a93801af6385838e
muhammadkh4n <muhammadkh4n@gmail.com>
2024-11-28 19:11:34 +0500

    Change freq indicator update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -28,7 +28,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     {
       url: appUrl,
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: 'weekly',
       priority: 1,
     },
     ...servicesCollection?.serviceCollection.items.map((service) => ({
